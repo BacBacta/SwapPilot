98d7bb06a61cd23d38d06277db4837aa1310da17
Bacta <tsannangcyrille@gmail.com>
2026-01-05 16:00:06 +0000

    fix(nav): include LandioNav component in LandioTemplate

--- apps/web/components/landio/landio-template.tsx
+++ apps/web/components/landio/landio-template.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from "react";
+import { LandioNav } from "./landio-nav";
 
 export function LandioTemplate({ inlineCss, bodyHtml, after }: { inlineCss?: string; bodyHtml: string; after?: ReactNode }) {
   return (
     <>
       {inlineCss ? <style dangerouslySetInnerHTML={{ __html: inlineCss }} /> : null}
+      <LandioNav />
       <div dangerouslySetInnerHTML={{ __html: bodyHtml }} />
       {after ?? null}
     </>
